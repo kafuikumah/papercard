96aca74c7d055a932a8b2a0bcc6d0c71d123d8b7
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-05-11 16:04:36 +0000

    Set up hero section of waitlist

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ import Waitlist from './components/Waitlist';
 
 function App() {
   return (
-    <Waitlist heroText="This application does things" buttonType="join the waitlist" />
+    
+    <div className='container'>
+      <Navigation />
+
+      <Waitlist
+    heroHeading="Your Ultimate Tech Starter Pack"
+    heroDescription="We are building a tech career toolbox filled with resources to help
+    tech professionals who are new, old or freelancers to get their documents and career progression right!"
+    joinWaitlistButton="Join Waitlist"
+    learnMoreButton="Learn More"
+    />
+    </div>
   );
 }
 
--- src/components/Waitlist.js
+++ src/components/Waitlist.js
@@ -1,8 +1,27 @@
 import React from "react";
+import '../styles/Waitlist.css'
 
 function Waitlist(props) {
     console.log(props);
-    return <h1>Waitlist, {props.heroText}, {props.buttonType}</h1>;
+    return (
+        <div>
+            <div>
+                <h1 className="waitlist-heading">{props.heroHeading}</h1>
+            </div>
+            
+            <div>
+                <h2 className="waitlist-sub-heading">
+                    {props.heroDescription}
+                </h2>
+            </div>
+
+            <div className="waitlist-button">
+                <button type="button">{props.joinWaitlistButton}</button>
+                <button type="button">{props.learnMoreButton}</button>
+            </div>
+    
+        </div>
+    );
 }
 
 export default Waitlist;
