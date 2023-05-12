7aead79d4b96737156da094da2f714a336ee6d3a
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-05-12 01:07:25 +0000

    Create Navigation bar and footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import './components/Navigation';
 import Navigation from './components/Navigation';
 import Waitlist from './components/Waitlist';
@@ -10,14 +11,19 @@ function App() {
     
     <div className='container'>
 
-      <Waitlist
+      <Navigation />
+
+
+      {/* <Waitlist
         heroHeading="Your Ultimate Tech Starter Pack"
         heroDescription="We are building a tech career toolbox filled with resources to help
         tech professionals who are new, old or freelancers to get their documents and career progression right!"
         joinWaitlistButton="Join Waitlist"
         learnMoreButton="Learn More"
-      />
-      <Navigation />
+      /> */}
+
+
+      <Footer />
 
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Footer() {
-    return <h1>Footer</h1>;
+    return (
+
+        <div>
+
+            <section class="py-10 bg-gray-50 sm:pt-16 lg:pt-24">
+                <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                    
+                    <hr class="mt-16 mb-10 border-gray-200" />
+
+                    <p class="text-sm text-center text-gray-600">© 2023. Designed and Developed by Kafui and Hilary, for tech professionals  </p>
+                </div>
+            </section>
+
+
+        </div>
+
+    );
 }
 
 export default Footer;
