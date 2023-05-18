e100fd095d7f3202a36f9548cecd35bf97665bcb
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-05-18 15:13:24 +0000

    Add Main.js for main application

---
+++ src/pages/Main.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from '../components/Footer';
+import Navigation from '../components/Navigation';
+
+
+
+function Main() {
+  return (
+
+    <div className='container mx-auto'>
+
+      <Navigation />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Main;
