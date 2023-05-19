4d6ae31c51299a7ac9774d05cd869bcc1ea9e913
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-05-19 15:10:45 +0000

    Refactor codebase wai

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import '../App.css';
+import '../styles/App.css';
 import Footer from '../components/Footer';
 import Navigation from '../components/Navigation';
 import Welcome from '../components/Welcome';
