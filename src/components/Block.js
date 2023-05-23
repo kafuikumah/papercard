7506fe9ac7ef214a00ae5cfb61a057f96276b51c
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-05-23 09:18:40 +0000

    set up button layout for block

---
+++ src/components/Block.js
@@ -0,0 +1,19 @@
+function Block() {
+
+    <div className="block">
+        <div className="block-container">
+            <div className="block-frame">
+                <div className="mb-10">
+                    <h3></h3>
+                    <p>These tools and resources have been recommended by many developers ccross the world</p>
+                </div>
+                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Check them out <span aria-hidden="true">→</span>
+              </a>
+            </div>
+        </div>
+    </div>
+
+}
+
+export default Block;
